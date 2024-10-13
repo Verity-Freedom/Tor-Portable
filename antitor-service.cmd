f851f893c72e30fe968be6f79a78d1e93b904fb7
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2024-10-13 19:06:42 +0300

    Add files via upload
    Убран лишний код.

--- antitor-service.cmd
+++ antitor-service.cmd
@@ -14,7 +14,6 @@ sc delete "Tor Win32 Service"
 if %errorLevel% NEQ 0 (
     powershell -Command "(gc '%CD%\torrc.txt') "^
       "-replace '^CacheDirectory.*$', ('CacheDirectory \"%CD%\data\"' -replace '\\','\\') "^
-      "-replace '^ClientOnionAuthDir.*$', ('ClientOnionAuthDir \"%CD%\data\onion-auth\"' -replace '\\','\\') "^
       "-replace '^DataDirectory.*$', ('DataDirectory \"%CD%\data\"' -replace '\\','\\') "^
       "-replace '^GeoIPFile.*$', ('GeoIPFile \"%CD%\data\geoip\"' -replace '\\','\\') "^
       "-replace '^GeoIPv6File.*$', ('GeoIPv6File \"%CD%\data\geoip6\"' -replace '\\','\\') "^
