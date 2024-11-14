f056fc7357eed86dad02ab454de61b4590c3453c
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2024-11-14 09:03:15 +0300

    Add files via upload

--- antitor-service.cmd
+++ antitor-service.cmd
@@ -1,15 +1,67 @@
-@set @_cmd=1 /*
-@echo off
+::::::::::::::::::::::::::::::::::::::::::::
+:: Elevate.cmd - Version 7
+:: Automatically check & get admin rights
+:: see "https://stackoverflow.com/a/12264592/1016343" for description
+::::::::::::::::::::::::::::::::::::::::::::
+ @echo off
+ CLS
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin shell
+ ECHO =============================
 
-whoami /groups | findstr "S-1-16-12288" >nul && goto :admin
-if "%~1"=="RunAsAdmin" goto :error
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~dpnx0"
+ rem this works also from cmd shell, other than %~0
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
 
-echo Requesting privileges elevation for managing Tor Win32 Service . . .
-cscript /nologo /e:javascript "%~f0" || goto :error
-exit /b
+:checkPrivileges
+  whoami /groups /nh | find "S-1-16-12288" > nul
+  if '%errorlevel%' == '0' ( goto checkPrivileges2 ) else ( goto getPrivileges )
 
-:admin
-pushd "%~dp0"
+
+:checkPrivileges2
+  net session 1>nul 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+  
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ ::::::::::::::::::::::::::::
+ ::START
+ ::::::::::::::::::::::::::::
 
 )
 
@@ -27,9 +79,4 @@ if %errorLevel% NEQ 0 (
     sc start "Tor Win32 Service"
 )
 
-pause */
-
-// JScript, restart batch script as administrator
-var objShell = WScript.CreateObject('Shell.Application');
-var ComSpec = WScript.CreateObject('WScript.Shell').ExpandEnvironmentStrings('%ComSpec%');
-objShell.ShellExecute(ComSpec, '/c ""' + WScript.ScriptFullName + '" RunAsAdmin"', '', 'runas', 1);
+pause
