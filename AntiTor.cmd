b8408d6c10bcdbe4791510a26ce637f20e8937f3
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-05-06 04:27:57 +0300

    Add files via upload

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -1,83 +1,10 @@
-@echo off
-chcp 1251
-set "serviceName=Tor Win32 Service"
-sc query "%serviceName%" | find "RUNNING" >nul
-
+sc query "Tor Win32 Service" | find "RUNNING"
 if %errorLevel% EQU 0 (
-::::::::::::::::::::::::::::::::::::::::::::
-:: Elevate.cmd - Version 9 custom
-:: Automatically check & get admin rights
-:: see "https://stackoverflow.com/a/12264592/1016343" for description
-::::::::::::::::::::::::::::::::::::::::::::
- CLS
- ECHO.
- ECHO =============================
- ECHO Running Admin shell
- ECHO =============================
-
-:init
- setlocal DisableDelayedExpansion
- set cmdInvoke=1
- set winSysFolder=System32
- set "batchPath=%~dpnx0"
- rem this works also from cmd shell, other than %~0
- for %%k in (%0) do set batchName=%%~nk
- set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
- setlocal EnableDelayedExpansion
-
-:checkPrivileges
-  whoami /groups /nh | find "S-1-16-12288" > nul
-  if '%errorlevel%' == '0' ( goto checkPrivileges2 ) else ( goto getPrivileges )
-
-
-:checkPrivileges2
-  net session 1>nul 2>NUL
-  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
-
-:getPrivileges
-  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
-  ECHO.
-  ECHO **************************************
-  ECHO Invoking UAC for Privilege Escalation
-  ECHO **************************************
-
-  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
-  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-  ECHO Next >> "%vbsGetPrivileges%"
-  
-  if '%cmdInvoke%'=='1' goto InvokeCmd 
-
-  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-  goto ExecElevation
-
-:InvokeCmd
-  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
-  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-
-:ExecElevation
- "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
- exit /B
-
-:gotPrivileges
- setlocal & cd /d %~dp0
- if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
-
- ::::::::::::::::::::::::::::
- ::START
- ::::::::::::::::::::::::::::
-   sc stop "Tor Win32 Service"
-   sc delete "Tor Win32 Service"
-   powershell -Command "(gc '%CD%\torrc.txt') "^
-  "-replace '^DataDirectory.*$', ('DataDirectory ..\data') "^
-  "-replace '^GeoIPFile.*$', ('GeoIPFile ..\data\geoip') "^
-  "-replace '^GeoIPv6File.*$', ('GeoIPv6File ..\data\geoip6') "^
-  "| Out-File -encoding default '%CD%\torrc.txt'"
-   TIMEOUT /T 1 /nobreak
-   cd tor
-   start /min tor -f ..\torrc.txt
+call service-manager.cmd
+TIMEOUT /T 2
+cd tor
+start /min tor -f ..\torrc.txt
 ) else (
-   cd tor
-   start /min tor -f ..\torrc.txt
+cd tor
+start /min tor -f ..\torrc.txt
 )
