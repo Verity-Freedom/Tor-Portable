b95bc76e617ef9b05f81d68a29bdb786b790464d
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-05-22 17:09:50 +0300

    Add files via upload

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -2,9 +2,6 @@ sc query "Tor Win32 Service" | find "RUNNING"
 if %errorLevel% EQU 0 (
 call service-manager.cmd
 TIMEOUT /T 2
-cd tor
-start /min tor -f ../torrc.txt
-) else (
-cd tor
-start /min tor -f ../torrc.txt
 )
+cd tor
+start /min tor -f ../torrc.txt
