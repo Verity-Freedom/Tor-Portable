7d8dda89690dcc01b6fb668a1308550d56cc4440
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-05-23 23:56:05 +0300

    Add files via upload

--- updater.sh
+++ updater.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 killall tor
-wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/Autoupdate/pro.zip -O ./change-mode/pro.zip
+wget https://ipfs.io/ipns/51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/Autoupdate/pro.zip -O ./change-mode/pro.zip
+if [ $? -eq 0 ]; then
+read -p "Pro mode updated successfully! Press Enter to continue updating or close the window now."
+else read -p "An unexpected error occured! Aborting..."
+exit
+fi
 wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/Autoupdate/geoip -O ./data/geoip
 wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/Autoupdate/geoip6 -O ./data/geoip6
 wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/Autoupdate/bridges-webtunnel-list.txt -O ./bridges-webtunnel-list.txt
@@ -13,4 +18,4 @@ wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfi
 wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/Autoupdate/AntiTor_linux/service-check.sh -O ./service-check.sh
 wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/Autoupdate/AntiTor_linux/service-create.sh -O ./service-create.sh
 
-sleep 10
+read -p "All files updated successfully!"
