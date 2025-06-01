a54381ee9f0d3ca02e616724d6b6ed6bf6a72d23
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-06-01 17:45:19 +0300

    Add files via upload

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -102,7 +102,7 @@ copy "%CD%\oldwin\acryptprimitives.dll" "C:\Windows\System32\acryptprimitives.dl
 sc query "Tor Win32 Service" | find "RUNNING"
 if %errorLevel% EQU 0 (
 call service-manager.cmd
-timeout /t 2 /nobreak
+timeout /t 3 /nobreak
 )
 cd tor
 start /min tor -f ../torrc.txt
