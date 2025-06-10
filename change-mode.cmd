d53730e5b1f3303ec6f79d3e6f27112ad408e6cf
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-06-10 18:09:56 +0300

    Add files via upload

--- change-mode.cmd
+++ change-mode.cmd
@@ -1,4 +1,10 @@
 @echo off
+taskkill /im tor.exe >nul 2>&1
+sc query "Tor Win32 Service" | find "RUNNING"
+if %errorLevel% EQU 0 (
+call service-manager.cmd
+timeout /t 3 /nobreak
+)
 findstr /c:"The mode is default" torrc.txt
 if %errorLevel% EQU 0 (
 copy "%CD%\change-mode\modes\pro\torrc.txt" "%CD%\torrc.txt"
