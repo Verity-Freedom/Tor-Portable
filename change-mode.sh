6c7478f673d1db1a08302c93914c309d5b27aac3
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-06-10 17:46:37 +0300

    Add files via upload

--- change-mode.sh
+++ change-mode.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-grep "The mode is default" ./torrc.txt
+grep "The mode is default" torrc.txt
 if [ $? -eq 0 ]; then
-cp -fr ./change-mode/modes/pro/torrc.txt ./torrc.txt
+cp -fr ./change-mode/modes/pro/torrc.txt torrc.txt
 read -p "The mode was changed to pro."
 exit
 fi
 
-grep "The mode is pro" ./torrc.txt
+grep "The mode is pro" torrc.txt
 if [ $? -eq 0 ]; then
-cp -fr ./change-mode/modes/default/torrc.txt ./torrc.txt
+cp -fr ./change-mode/modes/default/torrc.txt torrc.txt
 read -p "The mode was changed to default."
 exit
 else
-cp -fr ./change-mode/modes/default/torrc.txt ./torrc.txt
-read -p "The error occurred. Attempted to update the mode and set it to default. The error should be gone."
+cp -fr ./change-mode/modes/default/torrc.txt torrc.txt
+read -p "An error occurred. Attempted to update the mode and set it to default. The error should be gone."
 fi
