1e14393a520cb35b810cff24b8243580cf4459ee
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-08-13 06:11:43 +0300

    Add files via upload
    Simplify change mode structure.

--- change-mode.sh
+++ change-mode.sh
@@ -5,17 +5,17 @@ systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
 grep -q "The mode is default" torrc.txt
 if [ $? -eq 0 ]; then
-cp -fr ./change-mode/modes/pro/torrc.txt torrc.txt
+cp -fr ./change-mode/pro/torrc.txt torrc.txt
 read -p "The mode was changed to pro."
 exit
 fi
 
 grep -q "The mode is pro" torrc.txt
 if [ $? -eq 0 ]; then
-cp -fr ./change-mode/modes/default/torrc.txt torrc.txt
+cp -fr ./change-mode/default/torrc.txt torrc.txt
 read -p "The mode was changed to default."
 exit
 else
-cp -fr ./change-mode/modes/default/torrc.txt torrc.txt
+cp -fr ./change-mode/default/torrc.txt torrc.txt
 read -p "An error occurred. Attempted to update the mode and set it to default. The error should be gone."
 fi
