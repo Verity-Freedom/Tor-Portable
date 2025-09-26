e110b04b721c382387564ee3824c8315ff81beed
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-26 05:44:18 +0300

    Add files via upload

--- discord.sh
+++ discord.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+if command -v discord
+then
 LD_PRELOAD=./libdiscordbypass.so discord --proxy-server=socks5://127.0.0.1:9050
+else
+LD_PRELOAD=./libdiscordbypass.so vesktop --proxy-server=socks5://127.0.0.1:9050
+fi
