7a98c0c239a3fccb01c9f42328d2aba3fb25d3b8
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-28 15:56:33 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")"
 rm -r *
 wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_linux_current.zip ./AntiTor_linux_current.zip
 unzip ./AntiTor_linux_current.zip
