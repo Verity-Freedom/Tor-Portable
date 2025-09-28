e4002c35b2af431f814b3b6860dd9c60b927906d
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-28 06:12:17 +0300

    Add files via upload

--- discord.sh
+++ discord.sh
@@ -2,6 +2,20 @@
 if command -v discord
 then
 LD_PRELOAD=./libdiscordbypass.so discord --proxy-server=socks5://127.0.0.1:9050
-else
+fi
+if command -v vesktop
+then
 LD_PRELOAD=./libdiscordbypass.so vesktop --proxy-server=socks5://127.0.0.1:9050
+fi
+if command -v legcord
+then
+LD_PRELOAD=./libdiscordbypass.so legcord --proxy-server=socks5://127.0.0.1:9050
+fi
+if command -v webcord
+then
+LD_PRELOAD=./libdiscordbypass.so webcord --proxy-server=socks5://127.0.0.1:9050
+fi
+if command -v goofcord
+then
+LD_PRELOAD=./libdiscordbypass.so goofcord --proxy-server=socks5://127.0.0.1:9050
 fi
