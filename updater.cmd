8a03a64570d98aa3dde586bbe1dca69cae270964
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-28 18:07:52 +0300

    Update updater.cmd

--- updater.cmd
+++ updater.cmd
@@ -1,4 +1,10 @@
 cd /d %~dp0
+taskkill /im tor.exe >nul 2>&1
+sc query "Tor Win32 Service" | find "RUNNING"
+if %errorLevel% EQU 0 (
+call service-manager.cmd
+timeout /t 3 /nobreak
+)
 (
 echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win8+_current.zip', '%CD%\AntiTor_win8+_current.zip')"
 echo tar -xf "%CD%\AntiTor_win8+_current.zip"
@@ -7,3 +13,4 @@ echo del "%userprofile%\updater.cmd"
 ) > "%userprofile%\updater.cmd"
 start "" "%userprofile%\updater.cmd"
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
