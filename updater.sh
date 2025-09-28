0340ef53a51fd5940acb26f4d810e3d3db5c6662
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-28 18:08:36 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+lsof -t ./tor/ld-linux-x86-64.so.2 | xargs kill
+systemctl --user disable tor.service --now
+rm ~/.config/systemd/user/tor.service
 rm -r *
 wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_linux_current.zip ./AntiTor_linux_current.zip
 unzip ./AntiTor_linux_current.zip
