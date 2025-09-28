a2e7d8d374a0560b19cd94e79081eb28260f8ad6
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-28 15:43:30 +0300

    Add files via upload

---
+++ updater.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+rm -r *
+wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_linux_current.zip ./AntiTor_linux_current.zip
+unzip ./AntiTor_linux_current.zip
+rm ./AntiTor_linux_current.zip
+read -p ""
