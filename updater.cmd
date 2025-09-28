0760222545b61582e031939822a05892af2d412b
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-28 16:12:59 +0300

    Add files via upload
    Да вот теперь это индустриальный стандарт.

--- updater.cmd
+++ updater.cmd
@@ -1,6 +1,6 @@
 (
 echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win8+_current.zip', '%CD%\AntiTor_win8+_current.zip')"
-echo tar -xf "%CD%\AntiTor_win8+_current.zip"
+echo tar -xf "%CD%\AntiTor_win8+_current.zip" -C %~dp0
 echo del "%CD%\AntiTor_win8+_current.zip"
 echo del "%userprofile%\updater.cmd"
 ) > "%userprofile%\updater.cmd"
