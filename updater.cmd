3eddf0bbd2bc6482d7191ba2952f7d26916b11c5
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-30 05:51:38 +0300

    Update updater.cmd
    Use invoke web request instead.

--- updater.cmd
+++ updater.cmd
@@ -6,11 +6,12 @@ call service-manager.cmd
 timeout /t 3 /nobreak
 )
 (
-echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win8+_current.zip', '%CD%\AntiTor_win8+_current.zip')"
+echo powershell -Command "Invoke-WebRequest https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win8+_current.zip -OutFile AntiTor_win8+_current.zip"
 echo tar -xf "%CD%\AntiTor_win8+_current.zip"
 echo del "%CD%\AntiTor_win8+_current.zip"
 echo del "%userprofile%\updater.cmd"
 ) > "%userprofile%\updater.cmd"
 start "" "%userprofile%\updater.cmd"
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
 
