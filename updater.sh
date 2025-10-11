0d29f697f330b354919c48f54f00f138af9eabdf
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-10-11 15:14:48 +0300

    Update updater.sh
    Use curl everywhere.

--- updater.sh
+++ updater.sh
@@ -4,6 +4,6 @@ lsof -t ./tor/ld-linux-x86-64.so.2 | xargs kill
 systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
 rm -r *
-wget https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_linux_current.zip ./AntiTor_linux_current.zip
+curl https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_linux_current.zip -O
 unzip ./AntiTor_linux_current.zip
 rm ./AntiTor_linux_current.zip
