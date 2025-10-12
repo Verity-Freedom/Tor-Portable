8247abed03d73f11b677b0cfe5a4c20371ceb887
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-10-13 01:37:29 +0300

    Update AntiTor.cmd
    Simplify the code.

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -102,7 +102,7 @@ copy "%CD%\oldwin\acryptprimitives.dll" "C:\Windows\System32\acryptprimitives.dl
 
 :Execution
 cd /d %~dp0
-sc query "Tor Win32 Service" | find "RUNNING"
+sc query "Tor Win32 Service"
 if %errorLevel% EQU 0 (
 call service-manager.cmd
 timeout /t 3 /nobreak
