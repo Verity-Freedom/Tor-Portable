def7a40d24de3ded969b8f47752a98e1a0512b0a
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-10-17 18:13:48 +0300

    Update updater-win10+.cmd
    Fix updater breaking sometimes.

--- updater-win10+.cmd
+++ updater-win10+.cmd
@@ -14,4 +14,5 @@ echo del "%userprofile%\updater.cmd" >nul
 echo exit
 ) > "%userprofile%\updater.cmd"
 start "" "%userprofile%\updater.cmd"
+timeout /t 1 /nobreak
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
