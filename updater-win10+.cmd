27072a8e06b7f0c8120aa00f7741491fae71bf7f
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-01 12:40:48 +0300

    Update updater-win10+.cmd
    Better data saving.

--- updater-win10+.cmd
+++ updater-win10+.cmd
@@ -9,12 +9,12 @@ timeout /t 3 /nobreak
 echo curl.exe https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win8+_current.zip -O
 echo tar -xf "%CD%\AntiTor_win8+_current.zip"
 echo del "%CD%\AntiTor_win8+_current.zip"
-echo xcopy "%userprofile%\user-config" "%CD%\user-config" /i /e
-echo rmdir "%userprofile%\user-config" /s /q
+echo xcopy "%userprofile%\data" "%CD%\data" /i /e
+echo rmdir "%userprofile%\data" /s /q
 echo start cmd /C del "%userprofile%\updater.cmd"
 echo exit
 ) > "%userprofile%\updater.cmd"
-copy "%CD%\torrc.txt" "%CD%\user-config\torrc.txt"
-xcopy "%CD%\user-config" "%userprofile%\user-config" /i /e
+copy "%CD%\torrc.txt" "%CD%\data\torrc.txt"
+xcopy "%CD%\data" "%userprofile%\data" /i /e
 start "" "%userprofile%\updater.cmd"
 rmdir "%CD%" /s /q
