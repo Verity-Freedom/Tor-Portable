5e75309fe34a595816895361b9f3ded0dcf368ec
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-08 05:50:11 +0300

    Update updater.sh
    They are needed for & in links. Fix Google drive compatibility.

--- updater.sh
+++ updater.sh
@@ -6,7 +6,7 @@ rm ~/.config/systemd/user/tor.service
 cp ./torrc.txt ./data/torrc.txt
 cp -r ./data ~/data
 rm -r *
-curl https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_linux_current.zip -O
+curl "https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_linux_current.zip" -O
 unzip ./AntiTor_linux_current.zip
 rm ./AntiTor_linux_current.zip
 cp -r ~/data ./
