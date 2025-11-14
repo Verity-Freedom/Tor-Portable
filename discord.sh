ad6c09b5c78aebbbf0917b42828e7966c9438bd2
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-15 02:06:36 +0300

    Update discord.sh

--- discord.sh
+++ discord.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
+export BYPASS_DELAY=50
+export BYPASS_FAKE_PACKETS=2
+export LD_PRELOAD=./libdiscordbypass.so
 if command -v discord
 then
-LD_PRELOAD=./libdiscordbypass.so BYPASS_DELAY=50 BYPASS_FAKE_PACKETS=2 discord --proxy-server=socks5://127.0.0.1:9050
+discord --proxy-server=socks5://127.0.0.1:9050
 fi
 if command -v vesktop
 then
-LD_PRELOAD=./libdiscordbypass.so BYPASS_DELAY=50 BYPASS_FAKE_PACKETS=2 vesktop --proxy-server=socks5://127.0.0.1:9050
+vesktop --proxy-server=socks5://127.0.0.1:9050
 fi
 if command -v legcord
 then
-LD_PRELOAD=./libdiscordbypass.so BYPASS_DELAY=50 BYPASS_FAKE_PACKETS=2 legcord --proxy-server=socks5://127.0.0.1:9050
+legcord --proxy-server=socks5://127.0.0.1:9050
 fi
 if command -v webcord
 then
-LD_PRELOAD=./libdiscordbypass.so BYPASS_DELAY=50 BYPASS_FAKE_PACKETS=2 webcord --proxy-server=socks5://127.0.0.1:9050
+webcord --proxy-server=socks5://127.0.0.1:9050
 fi
 if command -v goofcord
 then
-LD_PRELOAD=./libdiscordbypass.so BYPASS_DELAY=50 BYPASS_FAKE_PACKETS=2 goofcord --proxy-server=socks5://127.0.0.1:9050
+goofcord --proxy-server=socks5://127.0.0.1:9050
 fi
