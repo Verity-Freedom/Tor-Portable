c2f9711b9d19ffa0373f3177ebab3bca812a09e1
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-30 23:26:14 +0300

    Update updater.cmd
    Use temp instead of userprofile.

--- updater.cmd
+++ updater.cmd
@@ -8,24 +8,24 @@ timeout /t 3 /nobreak
 )
 (
 echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win8+_current.zip', '%WAY%\AntiTor_win8+_current.zip')"
-echo cscript "%userprofile%\extractor.vbs"
+echo cscript "%temp%\extractor.vbs"
 echo exit
-)>"%userprofile%\updater.cmd"
+)>"%temp%\updater.cmd"
 (
 cmd /u /c echo CreateObject("Shell.Application"^).NameSpace("%CD%"^).CopyHere(CreateObject("Shell.Application"^).NameSpace("%WAY%\AntiTor_win8+_current.zip"^).items^)
-cmd /u /c echo CreateObject("WScript.Shell"^).Run "%userprofile%\cleaner.cmd"
-)>"%userprofile%\extractor.vbs"
+cmd /u /c echo CreateObject("WScript.Shell"^).Run "%temp%\cleaner.cmd"
+)>"%temp%\extractor.vbs"
 (
 echo del "%WAY%\AntiTor_win8+_current.zip"
-echo xcopy "%userprofile%\data" "%CD%\data" /i /e
-echo rmdir "%userprofile%\data" /s /q
+echo xcopy "%temp%\data" "%CD%\data" /i /e
+echo rmdir "%temp%\data" /s /q
 echo findstr /c:"The mode is pro" "%CD%\data\torrc.txt"
 echo if %%errorLevel%% EQU 0 copy "%CD%\change-mode\pro\torrc.txt" "%CD%\torrc.txt"
-echo cmd /c del "%userprofile%\updater.cmd"
-echo cmd /c del "%userprofile%\extractor.vbs"
-echo cmd /c del "%userprofile%\cleaner.cmd"
-)>"%userprofile%\cleaner.cmd"
+echo cmd /c del "%temp%\updater.cmd"
+echo cmd /c del "%temp%\extractor.vbs"
+echo cmd /c del "%temp%\cleaner.cmd"
+)>"%temp%\cleaner.cmd"
 copy "%CD%\torrc.txt" "%CD%\data\torrc.txt"
-xcopy "%CD%\data" "%userprofile%\data" /i /e
-start "" "%userprofile%\updater.cmd"
+xcopy "%CD%\data" "%temp%\data" /i /e
+start "" "%temp%\updater.cmd"
 rmdir "%CD%" /s /q
