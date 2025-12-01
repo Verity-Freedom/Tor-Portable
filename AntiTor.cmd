95af9bfefd154392c051e33d5b88c2873256304d
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-01 08:10:06 +0300

    Update AntiTor.cmd

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -2,7 +2,8 @@
 for %%I in (VERSION*) do set "UPD=%%~nxI"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')"
 if %errorlevel% NEQ 0 (
-echo call "%CD%\updater.cmd">"%temp%\autoupdater.cmd"
+echo @echo off>"%temp%\autoupdater.cmd"
+echo call "%CD%\updater.cmd">>"%temp%\autoupdater.cmd"
 echo echo If you see this message, then the automatic update is most likely successful. You can wait or press any button to start the program.>>"%temp%\autoupdater.cmd"
 echo timeout /t 15>>"%temp%\autoupdater.cmd"
 echo call "%CD%\AntiTor.cmd">>"%temp%\autoupdater.cmd"
