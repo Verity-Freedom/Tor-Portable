64f4611721f37649fa101ea6532bd7d65db7f7f4
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-02 00:25:44 +0300

    Update AntiTor.cmd
    I was fixing this forever

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -1,4 +1,5 @@
 @echo off
+cd /d "%~dp0"
 setlocal EnableDelayedExpansion
 for %%I in (VERSION*) do set "UPD=%%~nxI"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')" >nul
@@ -10,7 +11,7 @@ echo call "%CD%\updater.cmd">>"%temp%\autoupdater.cmd"
 echo cls>>"%temp%\autoupdater.cmd"
 echo :Wait>>"%temp%\autoupdater.cmd"
 echo if not exist "%CD%\tor\tor.exe" GOTO Wait>>"%temp%\autoupdater.cmd"
-echo call "%CD%\AntiTor.exe">>"%temp%\autoupdater.cmd"
+echo call "%CD%\AntiTor.cmd">>"%temp%\autoupdater.cmd"
 echo del "%temp%\autoupdater.cmd" ^& exit>>"%temp%\autoupdater.cmd"
 start "" "%temp%\autoupdater.cmd"
 exit
@@ -119,7 +120,6 @@ REM :::::::::::::::::::::::::::::::::::::::::
 copy "%CD%\oldwin\acryptprimitives.dll" "C:\Windows\System32\acryptprimitives.dll"
 
 :Execution
-cd /d %~dp0
 sc query "Tor Win32 Service" >nul
 if %errorLevel% EQU 0 (
 call service-manager.cmd
