521b2ab7d853763c8f1059d9f4f7874018d76eb9
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-01 08:35:52 +0300

    Update service-manager.cmd

--- service-manager.cmd
+++ service-manager.cmd
@@ -65,6 +65,22 @@ REM :::::::::::::::::::::::::::::::::::::::::
  REM START
  REM :::::::::::::::::::::::::
  REM Run shell as admin (example) - put here code as you like
+for %%I in (VERSION*) do set "UPD=%%~nxI"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')"
+if %errorlevel% NEQ 0 (
+echo @echo off>"%temp%\autoupdater.cmd"
+echo call "%CD%\updater.cmd">>"%temp%\autoupdater.cmd"
+echo cls>>"%temp%\autoupdater.cmd"
+echo echo If you see this message, then the automatic update is most likely successful. You can wait or press any button to start the program.>>"%temp%\autoupdater.cmd"
+echo timeout /t 30>>"%temp%\autoupdater.cmd"
+echo call "%CD%\AntiTor.cmd">>"%temp%\autoupdater.cmd"
+echo exit>>"%temp%\autoupdater.cmd"
+start "" "%temp%\autoupdater.cmd"
+exit
+) else (
+del "%temp%\%UPD%"
+del "%temp%\autoupdater.cmd" >nul 2>&1
+)
 copy "%CD%\oldwin\acryptprimitives.dll" "C:\Windows\System32\acryptprimitives.dll" >nul 2>&1
 taskkill /im tor.exe >nul 2>&1
 sc query "Tor Win32 Service" >nul
