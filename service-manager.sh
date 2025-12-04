f3538dcf799aa469a35e42ab5e8c1babafe5d9ea
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-04 05:13:16 +0300

    Update service-manager.sh
    This is the way for some reason

--- service-manager.sh
+++ service-manager.sh
@@ -11,7 +11,7 @@ systemctl --user is-active --quiet tor.service
  if [ $INP != 0 ]; then
  ./updater.sh
  fi
- if [[ $INP != 0 && $CHECK -eq 0 ]]; then
+ if [[ $INP != 0 && $CHECK = 0 ]]; then
  exit
  fi
 fi
