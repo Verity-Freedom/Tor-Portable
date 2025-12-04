f34d97db78a03304efec9ad2371d132fcfd7d4e8
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-04 05:30:34 +0300

    Update AntiTor.sh
    Yes baby!

--- AntiTor.sh
+++ AntiTor.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+UPD=(VERSION*)
+curl "https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/$UPD" -f -s -o /dev/null
+if [ $? -eq 22 ]; then
+read -n 1 -p "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Press any key if you want to update or 0 to skip" INP
+ if [ $INP != 0 ]; then
+ ./updater.sh
+ fi
+fi
 cd tor
 ./ld-linux-x86-64.so.2 --library-path . ./tor -f ../torrc.txt
