2fc0b87d7809e3693739f6e86000a0d41ff654cb
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-05 09:11:09 +0300

    Add files via upload
    It was a deliberate choice as it gives useful information and feedback, so I'm not gonna do this, not like this

--- updater.sh
+++ updater.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 lsof -t ./tor/ld-linux-x86-64.so.2 | xargs kill
-systemctl --user is-active --quiet tor.service
-if [ $? -eq 0 ]; then
 systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
-fi
 cp ./torrc.txt ./data/torrc.txt
 cp -r ./data ~/data
 rm -r *
