2c7993398d7bd0a10a9c723525337097f02ea5ae
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-11 11:50:36 +0300

    Update ipns-republisher.sh
    Better loop

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -3,5 +3,9 @@ cd "$(dirname "$0")"
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs init
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs daemon &
 sleep 10
+while pgrep -x ld-linux-x86-64 > /dev/null
+do
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
 curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
+sleep 1d
+done
