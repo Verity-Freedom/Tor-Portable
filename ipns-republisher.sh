499d330bd41b23da3f9bc427eb505303f9d167ff
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-12 15:26:07 +0300

    Update ipns-republisher.sh
    Were you RUSHING or DRAGGING?

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs init
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs daemon &
-sleep 10
+sleep 20
 while pgrep -x ld-linux-x86-64 > /dev/null
 do
 ./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
