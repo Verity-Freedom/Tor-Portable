5d3c566206d3f7e5187b58b0a3e7b935ad5eccef
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-19 14:07:20 +0300

    Update AntiTor.cmd
    Simplify the elevation

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -53,9 +53,9 @@ if %os_ver% GEQ 06.2 GOTO Execution
 if exist "C:\Windows\System32\acryptprimitives.dll" GOTO Execution
 chcp 1251
 REM :::::::::::::::::::::::::::::::::::::::::
-REM Elevate.cmd - Version 9
+REM Elevate.cmd - Version 10
 REM Automatically check & get admin rights
-REM see "https://stackoverflow.com/a/12264592/1016343" for description
+REM see "https://stackoverflow.com/a/12264592" for description
 REM :::::::::::::::::::::::::::::::::::::::::
  
  CLS
@@ -64,10 +64,6 @@ REM :::::::::::::::::::::::::::::::::::::::::
  ECHO Running Admin shell
  ECHO =============================
 
-:init
- setlocal DisableDelayedExpansion
- set cmdInvoke=1
- set winSysFolder=System32
  set "batchPath=%~dpnx0"
  rem this works also from cmd shell, other than %~0
  for %%k in (%0) do set batchName=%%~nk
@@ -77,7 +73,6 @@ REM :::::::::::::::::::::::::::::::::::::::::
 :checkPrivileges
   whoami /groups /nh | find "S-1-16-12288" > nul
   if '%errorlevel%' == '0' ( goto checkPrivileges2 ) else ( goto getPrivileges )
-
 
 :checkPrivileges2
   net session 1>nul 2>NUL
@@ -95,28 +90,20 @@ REM :::::::::::::::::::::::::::::::::::::::::
   ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
   ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
   ECHO Next >> "%vbsGetPrivileges%"
-  
-  if '%cmdInvoke%'=='1' goto InvokeCmd 
-
-  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-  goto ExecElevation
-
-:InvokeCmd
   ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
-  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\System32\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
 
-:ExecElevation
- "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ "%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
  exit /B
 
 :gotPrivileges
- setlocal & cd /d %~dp0
+ setlocal & cd /d "%~dp0"
  if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 
  REM :::::::::::::::::::::::::
  REM START
  REM :::::::::::::::::::::::::
- REM Run shell as admin (example) - put here code as you like
+ REM Run shell as admin - put your code below as you like
 copy "%CD%\oldwin\acryptprimitives.dll" "C:\Windows\System32\acryptprimitives.dll"
 
 :Execution
