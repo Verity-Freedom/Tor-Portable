8e4f0214eddac7a4212c15f704a81615c0ebc982
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-21 01:35:55 +0300

    Update service-manager.sh
    Nevermind I am definitely wrong

--- service-manager.sh
+++ service-manager.sh
@@ -4,8 +4,15 @@ UPD=(VERSION*)
 curl "https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/$UPD" -f -s -o /dev/null
 if [ $? -eq 22 ]; then
 read -n 1 -p "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Press any key if you want to update or 0 to skip" INP
+systemctl --user is-active --quiet tor.service
+ if [ $? -eq 0 ]; then
+ CHECK=0
+ fi
  if [ $INP != 0 ]; then
  ./updater.sh
+ fi
+ if [[ $INP != 0 && $CHECK = 0 ]]; then
+ exit
  fi
 fi
 if systemctl --user is-active --quiet tor.service; then
