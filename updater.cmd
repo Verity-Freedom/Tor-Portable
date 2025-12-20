7776e0fedb3b9efd0282822e9ee19fa197ffb03f
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-21 01:11:21 +0300

    Update updater.cmd
    People requested a simple service check for the updater (Keep It Functional, F!)

--- updater.cmd
+++ updater.cmd
@@ -3,26 +3,28 @@ if "%CD:~-1%" == "\" (set "WAY=%CD:~0,-1%") else set "WAY=%CD%"
 taskkill /im tor.exe >nul 2>&1
 sc query "Tor Win32 Service"
 if %errorLevel% EQU 0 (
+set "CHECK=0"
 call service-manager.cmd
 timeout /t 3 /nobreak
 )
 (
-echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win8+_current.zip', '%WAY%\AntiTor_win8+_current.zip')"
+echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/AntiTor_win7x64_current.zip', '%WAY%\AntiTor_win7x64_current.zip')"
 echo cscript "%temp%\extractor.vbs"
 echo exit
 )>"%temp%\updater.cmd"
 (
-cmd /u /c echo CreateObject("Shell.Application"^).NameSpace("%CD%"^).CopyHere(CreateObject("Shell.Application"^).NameSpace("%WAY%\AntiTor_win8+_current.zip"^).items^)
+cmd /u /c echo CreateObject("Shell.Application"^).NameSpace("%CD%"^).CopyHere(CreateObject("Shell.Application"^).NameSpace("%WAY%\AntiTor_win7x64_current.zip"^).items^)
 cmd /u /c echo CreateObject("WScript.Shell"^).Run "%temp%\cleaner.cmd"
 )>"%temp%\extractor.vbs"
 (
-echo del "%WAY%\AntiTor_win8+_current.zip"
+echo del "%WAY%\AntiTor_win7x64_current.zip"
 echo xcopy "%temp%\data" "%CD%\data" /i /e /y
 echo rmdir "%temp%\data" /s /q
 echo findstr /c:"The mode is pro" "%CD%\data\torrc.txt"
 echo if %%errorLevel%% EQU 0 copy "%CD%\change-mode\pro\torrc.txt" "%CD%\torrc.txt"
 echo del "%temp%\updater.cmd"
 echo del "%temp%\extractor.vbs"
+echo if "%CHECK%"=="0" call service-manager.cmd
 echo del "%temp%\cleaner.cmd"
 )>"%temp%\cleaner.cmd"
 copy "%CD%\torrc.txt" "%CD%\data\torrc.txt"
