cb7663cdb8bdcd0215a2db0ad5cdd2f8a999c521
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-20 20:35:57 +0300

    Update service-manager.cmd
    Why am I not surprised?

--- service-manager.cmd
+++ service-manager.cmd
@@ -12,8 +12,6 @@ REM :::::::::::::::::::::::::::::::::::::::::
  ECHO =============================
  ECHO.
 
- set "batchPath=%~dpnx0"
- rem this works also from cmd shell, other than %~0
  for %%k in (%0) do set batchName=%%~nk
  set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
 
@@ -28,7 +26,7 @@ REM :::::::::::::::::::::::::::::::::::::::::
   ECHO Invoking UAC for Privilege Escalation
   ECHO **************************************
  
-  ECHO CreateObject("Shell.Application").ShellExecute "%SystemRoot%\System32\cmd.exe", "/c""%batchPath%""", "", "runas", 1 >"%vbsGetPrivileges%"
+  ECHO CreateObject("Shell.Application").ShellExecute "%SystemRoot%\System32\cmd.exe", "/c""%~dpnx0""", "", "runas", 1 >"%vbsGetPrivileges%"
   ECHO CreateObject("Scripting.FileSystemObject").DeleteFile "%vbsGetPrivileges%">>"%vbsGetPrivileges%"
 
  "%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%"
