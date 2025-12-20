6db9839ed74a3c4a24e537388db43d8989d69066
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-20 14:37:06 +0300

    Add files via upload

---
+++ reset-oldwin-patch.cmd
@@ -0,0 +1,46 @@
+@echo off & cd /d "%~dp0"
+chcp 1251 >nul
+REM :::::::::::::::::::::::::::::::::::::::::
+REM Elevate.cmd - Version 10 custom
+REM Automatically check & get admin rights
+REM see "https://stackoverflow.com/a/79585058" for description
+REM :::::::::::::::::::::::::::::::::::::::::
+ 
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin shell
+ ECHO =============================
+ ECHO.
+
+ set "batchPath=%~dpnx0"
+ rem this works also from cmd shell, other than %~0
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+
+:checkPrivileges
+  whoami /groups /nh | find "S-1-16-12288" > nul
+  if not '%errorlevel%'=='0' goto getPrivileges
+  net session 1>nul 2>NUL
+  if '%errorlevel%'=='0' goto gotPrivileges
+
+:getPrivileges
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+ 
+  ECHO CreateObject("Shell.Application").ShellExecute "%SystemRoot%\System32\cmd.exe", "/c""%batchPath%""", "", "runas", 1 >"%vbsGetPrivileges%"
+  ECHO CreateObject("Scripting.FileSystemObject").DeleteFile "%vbsGetPrivileges%">>"%vbsGetPrivileges%"
+
+ "%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%"
+ exit
+
+:gotPrivileges
+REM :::::::::::::::::::::::::
+REM START
+REM :::::::::::::::::::::::::
+REM Run shell as admin - put your code below as you like
+del /F /Q "C:\Windows\System32\acryptprimitives.dll"
+if exist "C:\Windows\System32\acryptprimitives.dll" (2> nul set =) else (1> nul ver)
+if %errorLevel% EQU 0 echo The patch is not present anymore. You can try another Tor redaction if this one was not running, or try this one again.
+
+pause
