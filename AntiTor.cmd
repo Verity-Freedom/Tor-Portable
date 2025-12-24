f1b0e479804a6da5f0b97bc2d01cb964a9d7eb45
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-24 04:48:10 +0300

    Update AntiTor.cmd
    Powershell requires triple quotes for paths.

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -6,14 +6,14 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ip
 if %errorlevel% NEQ 0 (
 choice /m "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Do you want to update"
 if !errorlevel! EQU 2 GOTO Skip
-powershell -Command " '@echo off' | Out-File "%temp%\autoupdater.cmd" -encoding oem
-powershell -Command " 'call "%CD%\updater.cmd"' | Out-File "%temp%\autoupdater.cmd" -append -encoding oem
-powershell -Command " 'cls' | Out-File "%temp%\autoupdater.cmd" -append -encoding oem
-powershell -Command " ':Wait' | Out-File "%temp%\autoupdater.cmd" -append -encoding oem
-powershell -Command " 'if not exist "%CD%\torrc.txt" GOTO Wait' | Out-File "%temp%\autoupdater.cmd" -append -encoding oem
-powershell -Command " 'timeout /t 1 /nobreak' | Out-File "%temp%\autoupdater.cmd" -append -encoding oem
-powershell -Command " 'call "%CD%\%~nx0"' | Out-File "%temp%\autoupdater.cmd" -append -encoding oem
-powershell -Command " 'del "%temp%\autoupdater.cmd" & exit' | Out-File "%temp%\autoupdater.cmd" -append -encoding oem
+powershell -Command " '@echo off' | Out-File """%temp%\autoupdater.cmd""" -encoding oem
+powershell -Command " 'call """%CD%\updater.cmd"""' | Out-File """%temp%\autoupdater.cmd""" -append -encoding oem
+powershell -Command " 'cls' | Out-File """%temp%\autoupdater.cmd""" -append -encoding oem
+powershell -Command " ':Wait' | Out-File """%temp%\autoupdater.cmd""" -append -encoding oem
+powershell -Command " 'if not exist """%CD%\torrc.txt""" GOTO Wait' | Out-File """%temp%\autoupdater.cmd""" -append -encoding oem
+powershell -Command " 'timeout /t 1 /nobreak' | Out-File """%temp%\autoupdater.cmd""" -append -encoding oem
+powershell -Command " 'call """%CD%\%~nx0"""' | Out-File """%temp%\autoupdater.cmd""" -append -encoding oem
+powershell -Command " 'del """%temp%\autoupdater.cmd""" & exit' | Out-File """%temp%\autoupdater.cmd""" -append -encoding oem
 start "" "%temp%\autoupdater.cmd"
 exit
 )
