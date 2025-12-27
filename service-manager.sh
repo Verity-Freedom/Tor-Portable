2d8e622301fcd0c07884244b244a591c3bd4e9f3
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-27 18:05:34 +0300

    Update service-manager.sh
    It doesn't work as I want for some reason

--- service-manager.sh
+++ service-manager.sh
@@ -2,12 +2,16 @@
 cd "$(dirname "$0")"
 UPD=(VERSION*)
 curl "https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/$UPD" -f -s -o /dev/null
-if [[ $? -eq 22 ]]; then
-read -n 1 -p "The local version does not match the latest version. Press any key if you want to skip the update, 0 or 1 if you want to update, 0 if you want to start the service after update" INP
- if [[ $INP -eq 0 || $INP -eq 1 ]]; then
+if [ $? -eq 22 ]; then
+read -n 1 -p "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Press any key if you want to update or 0 to skip" INP
+systemctl --user is-active --quiet tor.service
+ if [ $? -eq 0 ]; then
+ CHECK=0
+ fi
+ if [ $INP != 0 ]; then
  ./updater.sh
  fi
- if [[ $INP -eq 1 ]]; then
+ if [[ $INP != 0 && $CHECK = 0 ]]; then
  exit
  fi
 fi
