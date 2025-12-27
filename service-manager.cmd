826dda109d35ec45010ce180fb7e2a5744214718
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-27 15:25:22 +0300

    Update service-manager.cmd
    Better logic

--- service-manager.cmd
+++ service-manager.cmd
@@ -36,30 +36,26 @@ REM :::::::::::::::::::::::::
 REM START
 REM :::::::::::::::::::::::::
 REM Run shell as admin - put your code below as you like
-sc query "Tor Win32 Service" >nul
-if %errorlevel% EQU 0 set "CHECK=0" & GOTO Service
-:Loop
 setlocal EnableDelayedExpansion
 for %%I in (VERSION*) do set "UPD=%%~nxI"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')" >nul
 if %errorlevel% NEQ 0 (
-choice /m "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Do you want to update"
-if !errorlevel! EQU 2 GOTO Skip
+choice /c abc /n /m "The local version does not match the latest version. Do you want to update and preserve service (A), update and remove service (B), or skip update (C)?"
+if !errorlevel! EQU 1 set "CHECK=0"
+if !errorlevel! EQU 3 GOTO Skip
 echo @echo off>"%temp%\autoupdater.cmd"
 echo call "%CD%\updater.cmd">>"%temp%\autoupdater.cmd"
 echo cls>>"%temp%\autoupdater.cmd"
 echo :Wait>>"%temp%\autoupdater.cmd"
 echo if not exist "%CD%\torrc.txt" GOTO Wait>>"%temp%\autoupdater.cmd"
 echo timeout /t 1 /nobreak>>"%temp%\autoupdater.cmd"
-echo if "%CHECK%" NEQ "1" call "%CD%\%~nx0">>"%temp%\autoupdater.cmd"
+echo if "%CHECK%" EQU "0" call "%CD%\%~nx0">>"%temp%\autoupdater.cmd"
 echo del "%temp%\autoupdater.cmd" ^& exit>>"%temp%\autoupdater.cmd"
 start "" "%temp%\autoupdater.cmd"
 exit
 )
 del "%temp%\%UPD%"
 :Skip
-If "%CHECK%"=="1" exit
-:Service
 if not exist "C:\Windows\System32\acryptprimitives.dll" copy "%CD%\oldwin\acryptprimitives.dll" "C:\Windows\System32\acryptprimitives.dll" >nul 2>&1
 taskkill /im tor.exe >nul 2>&1
 sc query "Tor Win32 Service" >nul
@@ -83,9 +79,4 @@ if %errorLevel% EQU 0 (
     sc start "Tor Win32 Service"
 )
 
-Echo.
-Echo Please don't close this window, I will finish the work and check version...
-Echo.
-timeout /t 3 /nobreak >nul
-
-If "%CHECK%"=="0" set "CHECK=1" & GOTO Loop
+timeout /t 3 /nobreak
