9a70596421fdb939e540e25f444a71b04d5678f0
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-30 11:20:09 +0300

    Update AntiTor.cmd

--- AntiTor.cmd
+++ AntiTor.cmd
@@ -60,7 +60,7 @@ REM :::::::::::::::::::::::::::::::::::::::::
 :checkPrivileges
   %SystemRoot%\System32\whoami.exe /groups /nh | %SystemRoot%\System32\find.exe "S-1-16-12288" 1>nul
   if errorlevel 1 goto getPrivileges
-  %SystemRoot%\System32\net.exe session 1>nul 2>NUL
+  %SystemRoot%\System32\net.exe session >nul 2>&1
   if not errorlevel 1 goto gotPrivileges
 
 :getPrivileges
